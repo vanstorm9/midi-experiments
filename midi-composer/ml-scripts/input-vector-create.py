0662afe20d222f539fa531a0eb39b73306c79fbf
vanstorm9 <antlowhur@yahoo.com>
2016-09-03 02:06:21 -0400

    Trained and managed to overfit a model with 1000 epoch

--- midi-composer/ml-scripts/input-vector-create.py
+++ midi-composer/ml-scripts/input-vector-create.py
@@ -1,13 +1,23 @@
+# Used to form the song input vector that we will used as input for model.py
+
 import music21
 import matplotlib.pyplot as plt
+import numpy as np
 import os
 
-#fp = '../songs/beethoven_movement01.mid'
-fp = '../songs/world-is-mine.mid'
-#fp = '../songs/Suteki-Da-Ne.mid'
-#fp = '../songs/how-to-world-domination.mid'
+matrix_path = "../matrices/input/"
+slash = "/"
+dash = "-"
 
-tracknum = 1
+#fp = '../songs/beethoven_movement01.mid'
+#fp = '../songs/world-is-mine.mid'
+#fp = '../songs/how-to-world-domination.mid'
+fp = '../songs/Suteki-Da-Ne.mid'
+
+#name = "world-is-mine"
+#name = "how-to-world-domination"
+name = "suteki-da-ne"
+
 
 mf = music21.midi.MidiFile()
 mf.open(fp)
@@ -18,14 +28,15 @@ mf.close()
 x = []
 y = []
 
+mainCounter = 0
+prevPitch = 0
+
 for tracksNum in range (0, len(mf.tracks)):
     # Prints out the number of events in a track
     numOfEvents = len(mf.tracks[tracksNum].events)
     print numOfEvents
     
     count = 0
-    prevPitch = -1
-
     for eventInd in range(0,numOfEvents):
         
         # Tracks
@@ -34,25 +45,29 @@ for tracksNum in range (0, len(mf.tracks)):
 
         if trackType == 'NOTE_ON':
             #print track
-		if prevPitch != track.pitch:
-			y.append(track.pitch)
-			x.append(count)
-
-			prevPitch = track.pitch
-
-			count = count + 1
+	    if prevPitch != track.pitch:
+                y.append(track.pitch)
+                x.append(count)
+                count = count + 1
         '''
         elif trackType == 'NOTE_OFF':
             print track
         '''
-        
+      
+    save_path = matrix_path + name + slash + name 
+    x_set = '-x-' + str(mainCounter) + '.npy'
+    y_set = '-y-' + str(mainCounter) + '.npy'
+    
+    np.save(save_path + x_set , x)
+    np.save(save_path + y_set , y)
+ 
     plt.plot(x,y,'ro')
     plt.show()
 
     x = []
     y = []
             
-
+    mainCounter = mainCounter + 1
         
     
 
